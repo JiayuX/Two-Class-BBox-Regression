ef378612ddfbe30ff20c03497d21ad66a7201435
Dennis Xie <46074081+JiayuX@users.noreply.github.com>
2022-01-03 13:44:58 -0500

    Add files via upload

---
+++ image_util.py
@@ -0,0 +1,89 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import cv2 as cv
+import colorsys
+from PIL import Image, ImageDraw, ImageFont
+
+
+""" Display a single image with title """
+def display_image(image, title):
+	image = image.astype('uint8')
+	image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+	image = np.int32(image)
+	plt.imshow(image, vmin=0, vmax=255)
+	plt.title(title)
+	plt.show()
+
+
+""" Display a single image with bbox added """
+def draw_rect(image, cords, color = None):    
+    image = image.copy()
+    
+    cords = cords.reshape(-1,4)
+    if not color:
+        color = [255,255,255]
+    for cord in cords:
+        
+        pt1, pt2 = (cord[0], cord[1]) , (cord[2], cord[3])
+                
+        pt1 = int(pt1[0]), int(pt1[1])
+        pt2 = int(pt2[0]), int(pt2[1])
+    
+        image = cv.rectangle(image.copy(), pt1, pt2, color, int(max(image.shape[:2])/200))
+    return image
+
+
+""" Rescale the image """
+def rescaledFrame(frame, target_size=[600,600]):
+    return cv.resize(frame, (target_size[0], target_size[1]), interpolation=cv.INTER_AREA)
+
+
+"""
+Functions 'generate_colors' and 'draw_boxes' are dopted from Deep Learning Specialization on Coursera
+by Andrew Ng
+"""
+def generate_colors(class_names):
+    hsv_tuples = [(x / len(class_names), 1., 1.) for x in range(len(class_names))]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
+    random.seed(10101)  # Fixed seed for consistent colors across runs.
+    random.shuffle(colors)  # Shuffle colors to decorrelate adjacent classes.
+    random.seed(None)  # Reset seed to default.
+    return colors
+
+
+def draw_boxes(image, out_scores, out_boxes, out_classes, class_names, colors):
+    
+    font = ImageFont.truetype(font='font/FiraMono-Medium.otf',size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
+    thickness = (image.size[0] + image.size[1]) // 300
+
+    for i, c in reversed(list(enumerate(out_classes))):
+        predicted_class = class_names[c]
+        box = out_boxes[i]
+        score = out_scores[i]
+
+        label = '{} {:.2f}'.format(predicted_class, score)
+
+        draw = ImageDraw.Draw(image)
+        label_size = draw.textsize(label, font)
+
+        left, top, right, bottom = box
+        top = max(0, np.floor(top + 0.5).astype('int32'))
+        left = max(0, np.floor(left + 0.5).astype('int32'))
+        bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
+        right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
+        print(label, (left, top), (right, bottom))
+
+        if top - label_size[1] >= 0:
+            text_origin = np.array([left, top - label_size[1]])
+        else:
+            text_origin = np.array([left, top + 1])
+
+        # My kingdom for a good redistributable image drawing library.
+        for i in range(thickness):
+            draw.rectangle([left + i, top + i, right - i, bottom - i], outline=colors[c])
+        draw.rectangle([tuple(text_origin), tuple(text_origin + label_size)], fill=colors[c])
+        draw.text(text_origin, label, fill=(0, 0, 0), font=font)
+        del draw
+
